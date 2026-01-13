48743067b0fc067bd993bd6f08775c09a58398db
Ankan Manna <167167394+ankanmanna@users.noreply.github.com>
2026-01-13 17:09:37 +0530

    Add files via upload

---
+++ ContactForm/script.js
@@ -0,0 +1,13 @@
+
+// var value1 = "rahul das";
+// // console.log(value1);
+
+// value1 = "Pritam nath"
+
+// console.log(value1)
+
+let star = "*"
+
+for (let i = 1; i <= 5; i++) {
+  console.log("hello world");
+}
