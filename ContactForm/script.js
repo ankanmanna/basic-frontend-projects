be0e40ab39ccc35d19a03421f553fac714440aa1
ankanmanna <ankanmanna.dev@gmail.com>
2026-01-13 21:03:16 +0530

    Updated existing file

--- ContactForm/script.js
+++ ContactForm/script.js
@@ -1,13 +1,5 @@
 
-// var value1 = "rahul das";
-// // console.log(value1);
-
-// value1 = "Pritam nath"
-
-// console.log(value1)
-
-let star = "*"
-
-for (let i = 1; i <= 5; i++) {
-  console.log("hello world");
-}
+/*
+  This JavaScript file is intentionally left blank.
+  Code can be added here later if needed.
+*/
